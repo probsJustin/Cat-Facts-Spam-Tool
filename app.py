a1fe024540d5c78b33bf70986c90228dc5ce7d83
Justin Hagerty <wheelsmanx@gmail.com>
2021-04-24 21:32:42 -0400

    updated and threaded app

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from functools import partial
 from twilio.rest import Client
 import os
+from api import account_sid, auth_token
+import threading
 
 class gui_stuff(tk.Frame):
 
@@ -15,8 +17,8 @@ class gui_stuff(tk.Frame):
         self.pack()
         self.init_ui()
 
-    def init_ui(self):
 
+    def init_ui(self):
         self.buttons["send"] = tk.Button(self, borderwidth=10, command=partial(self.actions, "send"))
         self.buttons["send"].grid(row=1, column=0, padx=20, pady=20)
         self.buttons["send"]["text"] = "send"
@@ -28,15 +30,38 @@ class gui_stuff(tk.Frame):
         self.fields["phone_number"] = tk.Text(self, height=2, width=40)
         self.fields["phone_number"].grid(row=0, column=1, columnspan=3, padx=20, pady=20)
 
+        self.fields["amount_loop"] = tk.Text(self, height=2, width=10)
+        self.fields["amount_loop"].grid(row=3, column=1, padx=20, pady=20)
+
         self.labels["phone_number"] = tk.Label(self, borderwidth=10)
         self.labels["phone_number"].grid(row=0, column=0, padx=20, pady=20)
         self.labels["phone_number"]["text"] = "Phone Number"
 
-    def actions(self, action):
-        return_variable = action
-        if(action == "send"):
+        self.labels["amount_loop"] = tk.Label(self, borderwidth=20)
+        self.labels["amount_loop"].grid(row=3, column=0, padx=20, pady=20)
+        self.labels["amount_loop"]["text"] = "# of cat facts"
+
+    def threaded_sms_loop(self):
+        if (self.fields["amount_loop"].get("1.0", tk.END) == ""):
             print(type(self.fields["phone_number"].get("1.0", tk.END)))
             self.create_sms(self.get_cat_facts(), str(self.fields["phone_number"].get("1.0", tk.END)))
+
+        else:
+            counter = int(self.fields["amount_loop"].get("1.0", tk.END))
+            for x in range(0, counter):
+                try:
+                    print(type(self.fields["phone_number"].get("1.0", tk.END)))
+                    self.create_sms(self.get_cat_facts(), str(self.fields["phone_number"].get("1.0", tk.END)))
+                except Exception as e:
+                    print(e)
+
+
+    def actions(self, action):
+        return_variable = action
+        counter = 1
+        if(action == "send"):
+            sms_loop = threading.Thread(name='threaded_sms_loop', target=self.threaded_sms_loop)
+            sms_loop.start()
 
         if (action == "reset"):
             print(self.get_cat_facts())
@@ -47,14 +72,13 @@ class gui_stuff(tk.Frame):
 
         # Your Account Sid and Auth Token from twilio.com/console
         # and set the environment variables. See http://twil.io/secure
-        account_sid = "AC1e09ea82c62ff5d844472d65bf8a4e58"
-        auth_token = "57b97050ef022563dc9a08f105900e5a"
+
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(
-            body=text,
-            from_='+16143680119',
-            to= phone_number
+            body = text,
+            from_= '+16143680119',
+            to = phone_number
         )
 
 
